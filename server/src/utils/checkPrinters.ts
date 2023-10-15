fa2ff8d88c1559ca8a58ce1eb98a53b9f59b2929
Saj Arora <saj.arora24@gmail.com>
2023-10-15 19:27:57 -0400

    work on printer detection

--- server/src/utils/checkPrinters.ts
+++ server/src/utils/checkPrinters.ts
@@ -1,24 +1,10 @@
-import { exec } from 'child_process';
+import { findByIds, getDeviceList } from 'usb';
 
 function checkPrinters() {
-    // Using lpstat -a to list all connected printers
-    exec('lpstat -a', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Stderr: ${stderr}`);
-            return;
-        }
-
-        if (stdout) {
-            console.log('Printers found:');
-            console.log(stdout);
-        } else {
-            console.log('No printers found.');
-        }
-    });
+    const device = findByIds(6495, 1)
+    if (device) {
+        console.log(device.portNumbers, device.deviceDescriptor)
+    }
 }
 
 export default checkPrinters;
